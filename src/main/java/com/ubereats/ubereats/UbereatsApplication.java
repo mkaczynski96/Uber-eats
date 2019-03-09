2ecfdcb416e89c8795965fe93e254a666f80b60b
donmikele <kaczynskimichalkonrad@gmail.com>
2019-03-09 19:38:47 +0100

    Initial commit

---
+++ src/main/java/com/ubereats/ubereats/UbereatsApplication.java
@@ -0,0 +1,13 @@
+package com.ubereats.ubereats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UbereatsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UbereatsApplication.class, args);
+    }
+
+}

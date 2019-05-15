a3e090c10b0d97b83090c3fd9e3d58f59759b463
mkaczynski96 <kaczynskimichalkonrad@gmail.com>
2019-05-15 15:52:15 +0200

    Initializable with example data

--- uber-eats-web/src/main/java/com/ubereats/bootstrap/DataLoader.java
+++ uber-eats-web/src/main/java/com/ubereats/bootstrap/DataLoader.java
@@ -1,12 +1,12 @@
 package com.ubereats.bootstrap;
 
-import com.ubereats.model.Category;
-import com.ubereats.model.Client;
-import com.ubereats.model.Dish;
-import com.ubereats.model.Premises;
+import com.ubereats.model.*;
 import com.ubereats.services.*;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+import java.util.Set;
 
 @Component
 public class DataLoader implements CommandLineRunner {
@@ -52,6 +52,10 @@ public class DataLoader implements CommandLineRunner {
         americanFood.setName("American");
         Category savedAmericanFood = categoryService.save(americanFood);
 
+        Category pizzaFood = new Category();
+        pizzaFood.setName("Pizza");
+        Category savedPizzafood = categoryService.save(pizzaFood);
+
 
         Dish hamburger = new Dish();
         hamburger.setName("Hamburger");
@@ -60,13 +64,38 @@ public class DataLoader implements CommandLineRunner {
         hamburger.setQuantity(1);
         hamburger.setSize("Small");
         hamburger.setPremises(savedPremises1);
-        Dish savedHamburger = dishService.save(hamburger);
+        dishService.save(hamburger);
+
+        Dish margharita = new Dish();
+        margharita.setName("Margharita");
+        margharita.setCategory(savedPizzafood);
+        margharita.setPrice(30);
+        margharita.setQuantity(1);
+        margharita.setSize("XXL");
+        margharita.setPremises(savedPremises2);
+        dishService.save(margharita);
+
 
 
         Client client1 = new Client();
+        client1.setFirstName("Michal");
+        client1.setLastName("Katano");
         client1.setAddress("ul. Jana Krysta 6");
         client1.setPhoneNumber("793-156-322");
         client1.setRangeDistanceToLocal(15L);
+        clientService.save(client1);
+
+
+        Bill bill1 = new Bill();
+        bill1.setClient(client1);
+        bill1.setDelivery(true);
+        bill1.setDeliveryTime(30);
+        bill1.getDish().add(margharita);
+        bill1.setLocalDate(LocalDate.now());
+        bill1.setPaid(true);
+        bill1.setPremises(premises2);
+        bill1.setTotalAmount(30);
+        billService.save(bill1);
 
         System.out.println("Loaded data");
 
